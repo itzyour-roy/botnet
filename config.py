b99705cec705c30f336f764a94024a9f1f232948
NotyourRonkit <104563067+itzyour-roy@users.noreply.github.com>
2025-12-29 20:48:24 +0530

    Update bot configuration and tokens

---
+++ config.py
@@ -0,0 +1,20 @@
+TOKEN = ""  # Bot token
+CRYPTO = ""  # CryptoBot token
+ADMINS = [firstID, SecondID]  # Admin IDs
+
+# Bot Options
+bot_name = "yourBot"
+bot_logs = "-1002293289427"
+bot_channel_link = ""
+bot_admin = "@hxcker07,@hxcker07"
+bot_documentation = ""
+bot_works = ""
+bot_reviews = ""
+
+# Prices
+subscribe_1_day = 2
+subscribe_7_days = 7
+subscribe_14_days = 69.99
+subscribe_30_days = 15
+subscribe_365_days = 69.99
+subscribe_infinity_days = 30
